903c241a88404a3666afcfd4611d5881c7f8be01
Rishi-V <aiflab@bharaniMacbook.local>
2017-10-07 15:54:17 -0700

    Commiting to this

---
+++ app/src/main/java/com/example/aiflab/testfixit/IssueListViewActivity.java
@@ -0,0 +1,16 @@
+package com.example.aiflab.testfixit;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by aiflab on 10/6/17.
+ */
+
+public class IssueListViewActivity extends SingleFragmentActivity {
+    protected Fragment createFragment() {
+        return new IssueListViewFragment();
+    }
+}
+
+
+
